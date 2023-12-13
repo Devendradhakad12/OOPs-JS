1062dc24474614e72817ae32b165c31c6cf83992
Devendra Dhakad <121676745+Devendradhakad12@users.noreply.github.com>
2023-12-13 13:14:35 +0530

    constructor function

--- Constructor_Function/index.js
+++ Constructor_Function/index.js
@@ -2,7 +2,55 @@ function BanckAccount(customerName, balance = 0) {
   this.custmorName = customerName; // properties
   this.accountNumber = Date.now();
   this.balance = balance;
+
+  this.deposit = function (amount) {
+    this.balance += amount;
+  };
+  this.withdraw = function (amount) {
+    this.balance -= amount;
+  };
 }
 
-const devAccount = new BanckAccount("Dev",1000)
-console.log(devAccount)
+/* const devAccount = new BanckAccount("Dev", 1000);
+devAccount.withdraw(500);
+console.log(devAccount);
+
+const mahakAccount = new BanckAccount("Mahak");
+mahakAccount.deposit(5000);
+console.log(mahakAccount); */
+
+// Dom
+const accountForm = document.querySelector("#accountForm");
+const custmorName = document.querySelector("#customerName");
+const balance = document.querySelector("#balance");
+const Accounts = [];
+
+const depositForm = document.querySelector("#depositForm");
+const accountNumber = document.querySelector("#accountNumber");
+const depositAmount = document.querySelector("#depositBalance");
+
+// Create Account
+accountForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  if (custmorName.value === "") {
+    alert("All Field's required");
+    return;
+  }
+  const acount = new BanckAccount(custmorName.value, +balance.value);
+  Accounts.push(acount);
+  custmorName.value = "";
+  balance.value = "";
+  console.log(Accounts);
+});
+
+// Deposit Amount
+depositForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  if (accountNumber.value === "") {
+    alert("All Field's required");
+    return;
+  }
+ let account = Accounts.find((account)=> account?.accountNumber === Number(accountNumber.value))
+ account.deposit(Number(depositAmount.value))
+  console.log(Accounts);
+});
